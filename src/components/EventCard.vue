cc90315ed8e3c97b4d1e935e55a5508daf254a08
aureliasegarra <av.segarra@gmail.com>
2021-08-03 12:20:26 +0200

    css update on EventCard component

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,9 +1,11 @@
 <template>
-  <router-link :to="{ name: 'event-show', params: { id: '1' } }"
-    >Show Event #1
-    <div>
-      <span>@{{ event.time }} on {{ event.date }}</span>
-      <h4>{{ event.title }}</h4>
+  <router-link
+    class="event-link"
+    :to="{ name: 'event-show', params: { id: event.id } }"
+  >
+    <div class="event-card -shadow">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h4 class="title">{{ event.title }}</h4>
       <span>{{ event.attendees.length }} attending</span>
     </div>
   </router-link>
@@ -29,7 +31,22 @@ export default {
 </script>
 
 <style scoped>
-h4 {
-  color: green;
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
 }
 </style>
