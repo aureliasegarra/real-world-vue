19aae1e7856ef172b0d5a8937950bc83d6cca607
aureliasegarra <av.segarra@gmail.com>
2021-08-03 15:58:25 +0200

    global component

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BaseIcon from "@/components/BaseIcon.vue";
+import BaseButton from "@/components/BaseButton.vue";
+import BaseInput from "@/components/BaseInput.vue";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .component("BaseIcon", BaseIcon)
+  .component("BaseButton", BaseButton)
+  .component("BaseInput", BaseInput)
+  .mount("#app");
